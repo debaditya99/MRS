a3f185bb80c5cbdf7d1f9431d771e74b38feb2d5
debaditya99 <82333171+debaditya99@users.noreply.github.com>
2022-05-20 19:23:17 +0530

    Add files via upload

---
+++ community - Copy/blog/forms.py
@@ -0,0 +1,10 @@
+from .models import comment
+from django import forms
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = comment
+        fields = ('name','body')
+        widgets = {
+        'name': forms.TextInput(attrs={"placeholder": "E.g. John Bruh",'class':'form-control'}),
+        'body': forms.Textarea(attrs={"placeholder": "Write your views about this post.",'class':'form-control'}),
+        }
